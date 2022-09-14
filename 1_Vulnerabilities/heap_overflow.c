dc0d07638f6f14d0fe3aaa2b959abd73ba53e948
DeMarcus Thomas <demarcus.thomas601@gmail.com>
2022-09-14 16:53:39 -0500

    Create heap_overflow.c
    Added heap_overflow example.

---
+++ 1_Vulnerabilities/heap_overflow.c
@@ -0,0 +1,26 @@
+#include<stdlib.h>
+#include<string.h>
+#include<stdio.h>
+
+void heap_of(const char *x){
+
+    char *y = malloc(20);
+    strcpy(y,x);
+
+    printf("The input was: %s\n",y);
+}
+
+int main(int argc, char **argv){
+
+    heap_of(argv[1]);
+
+    return 0;
+}
+
+
+/*
+[afl++ 805609b77f1e] /src/fuzzing_tutorial/1_Vulnerabilities (main) # ./a.out hellohellohellohellohellohe
+malloc(): corrupted top size
+Aborted
+\[afl++ 805609b77f1e] /src/fuzzing_tutorial/1_Vulnerabilities (main) #
+*/

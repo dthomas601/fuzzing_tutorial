1074d761b62a5c3f731e17175dd547eb618cd322
DeMarcus Thomas <demarcus.thomas601@gmail.com>
2022-11-10 09:53:42 -0600

    Update printchars_driver.cpp
    Updated include statement.

--- 2_Fuzzing_Tools/libFuzzer/printchars_driver.cpp
+++ 2_Fuzzing_Tools/libFuzzer/printchars_driver.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include<string>
 
-#include "printchar.h"
+#include "printchars.h"
 #include <stdint.h>
 #include <stddef.h>
 
@@ -10,7 +10,6 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
 	
 	std::string s(reinterpret_cast<const char *>(data), size);
 	
-	//printf("Size of string: %zu, %s\n",size,s.c_str());		
 	printchars(s);
 
 	return 0;
